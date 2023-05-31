000fbef8077f79104fef325810abccc2a881d01f
Kipronokirui <victorkipronokirui2@gmail.com>
2023-05-31 21:03:27 +0300

    Token Authorization

--- reviews/reviews/urls.py
+++ reviews/reviews/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include 
 from rest_framework import routers
 from review import views
+from rest_framework_simplejwt import views as jwt_views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -29,5 +30,7 @@ router.register(r'categories', views.CategoryViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
